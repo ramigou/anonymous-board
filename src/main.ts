dd7004d6966ae283a9108017ed55140de91fe51a
ramigou <calmeramigou@gmail.com>
2024-09-08 15:16:42 +0900

    build(npm): set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+
+  const config = new DocumentBuilder()
+    .setTitle('Anonymous Board')
+    .setDescription('Anonymous Board API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
