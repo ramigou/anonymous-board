ca525e2862380efc6d55382bc26d7916160583cb
ramigou <calmeramigou@gmail.com>
2024-09-10 00:54:42 +0900

    test: add AAA pattern unit test cases

--- src/modules/comments/comments.controller.spec.ts
+++ src/modules/comments/comments.controller.spec.ts
@@ -1,20 +1,108 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { plainToInstance } from 'class-transformer';
+import { CreateCommentReqDto } from './dto/create-comment.req.dto';
+import { CreateCommentResDto } from './dto/create-comment.res.dto';
+import { FindCommentsResDto } from './dto/find-comment.res.dto';
+import { FindCommentsReqDto } from './dto/find-commnets.req.dto';
+import { Comment } from '../../entities/comment.entity';
 
 describe('CommentsController', () => {
-  let controller: CommentsController;
+  let commentsController: CommentsController;
+  let commentsService: CommentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
-      providers: [CommentsService],
+      providers: [
+        {
+          provide: CommentsService,
+          useValue: {
+            createComment: jest.fn(),
+            findAllComments: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<CommentsController>(CommentsController);
+    commentsController = module.get<CommentsController>(CommentsController);
+    commentsService = module.get<CommentsService>(CommentsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should create a comment and return the result', async () => {
+      // Arrange
+      const createCommentDto: CreateCommentReqDto = {
+        content: 'This is a comment',
+        author_name: 'john doe',
+        post_id: 1,
+      };
+
+      const newComment = {
+        id: 1,
+        ...createCommentDto,
+        created_at: new Date(),
+      } as unknown as Comment;
+
+      jest
+        .spyOn(commentsService, 'createComment')
+        .mockResolvedValue(newComment);
+
+      // Act
+      const result = await commentsController.create(createCommentDto);
+
+      // Assert
+      expect(commentsService.createComment).toHaveBeenCalledWith(
+        createCommentDto,
+      );
+      expect(result).toEqual(plainToInstance(CreateCommentResDto, newComment));
+    });
+  });
+
+  describe('findAllComments', () => {
+    it('should return comments and total count for a given post', async () => {
+      // Arrange
+      const findCommentsQuery: FindCommentsReqDto = {
+        post_id: 1,
+        page: 1,
+        limit: 5,
+      };
+
+      const comments = [
+        {
+          id: 1,
+          content: 'First comment',
+          author_id: 1,
+          post_id: 1,
+          create_at: new Date(),
+        },
+        {
+          id: 2,
+          content: 'Second comment',
+          author_id: 2,
+          post_id: 1,
+          created_at: new Date(),
+        },
+      ] as Comment[];
+
+      const total = 10;
+
+      jest
+        .spyOn(commentsService, 'findAllComments')
+        .mockResolvedValue({ comments, total });
+
+      // Act
+      const result =
+        await commentsController.findAllComments(findCommentsQuery);
+
+      // Assert
+      expect(commentsService.findAllComments).toHaveBeenCalledWith(
+        findCommentsQuery,
+      );
+      expect(result).toEqual(
+        plainToInstance(FindCommentsResDto, { comments, total }),
+      );
+    });
   });
 });
--- src/modules/keywords/keywords.service.spec.ts
+++ src/modules/keywords/keywords.service.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { KeywordsService } from './keywords.service';
+import { KeywordsRepository } from './keywords.repository';
+import { Keyword } from '../../entities/keyword.entity';
+import { Author } from '../../entities/author.entity';
 
 describe('KeywordsService', () => {
-  let service: KeywordsService;
+  let keywordsService: KeywordsService;
+  let keywordsRepository: KeywordsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [KeywordsService],
+      providers: [
+        KeywordsService,
+        {
+          provide: KeywordsRepository,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<KeywordsService>(KeywordsService);
+    keywordsService = module.get<KeywordsService>(KeywordsService);
+    keywordsRepository = module.get<KeywordsRepository>(KeywordsRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findAllKeywords', () => {
+    it('should return an array of keywords', async () => {
+      // Arrange
+      const keywords: Keyword[] = [
+        {
+          id: 1,
+          keyword: 'keyword1',
+          author_id: 1,
+          created_at: new Date(),
+          deleted_at: null,
+          author: new Author(),
+          notifications: [],
+        },
+        {
+          id: 2,
+          keyword: 'keyword2',
+          author_id: 2,
+          created_at: new Date(),
+          deleted_at: null,
+          author: new Author(),
+          notifications: [],
+        },
+      ];
+
+      jest.spyOn(keywordsRepository, 'findAll').mockResolvedValue(keywords);
+
+      // Act
+      const result = await keywordsService.findAllKeywords();
+
+      // Assert
+      expect(keywordsRepository.findAll).toHaveBeenCalled();
+      expect(result).toEqual(keywords);
+    });
   });
 });
